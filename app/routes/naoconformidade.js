88494cad0813317e211b291cce9bab16b99d05ad
Rodolfo Cruz <rodolfocruz.ti@gmail.com>
2016-06-12 23:17:09 -0300

    Ajustando

--- app/routes/naoconformidade.js
+++ app/routes/naoconformidade.js
@@ -1,25 +1,26 @@
-function fromHex(hex,str){
-  try{
-    str = decodeURIComponent(hex.replace(/(..)/g,'%$1'));
-  }
-  catch(e){
-    str = hex;
-    console.log('invalid hex input: ' + hex);
-  }
-  return str;
+function fromHex(hex){
+	var str;
+	try{
+		str = decodeURIComponent(hex.replace(/(..)/g,'%$1'));
+	}catch(e){
+		str = hex;
+		console.log('invalid hex input: ' + hex);
+	}
+	return str;
 }
 
-function toHex(str,hex){
-  try{
-    hex = unescape(encodeURIComponent(str)).split('').map(function(v){
-    	return v.charCodeAt(0).toString(16);
-    }).join('');
-  }
-  catch(e){
-    hex = str;
-    console.log('invalid text input: ' + str);
-  }
-  return hex;
+function toHex(str){
+	var hex;
+	try{
+		hex = unescape(encodeURIComponent(str)).split('').map(function(v){
+			return v.charCodeAt(0).toString(16);
+		}).join('');
+	}catch(e){
+		hex = str;
+		console.log('invalid text input: ' + str);
+	}
+	
+	return hex;
 }
 
 module.exports = function(app){
